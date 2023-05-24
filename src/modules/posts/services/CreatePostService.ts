2f4c560b68ee1c3e7a33b2af6cfc3c6a3728e136
Gabriel Dias <gabrieldiascorrea7@gmail.com>
2023-05-24 20:06:26 -0300

    refactor: Separate project into modules user and post

---
+++ src/modules/posts/services/CreatePostService.ts
@@ -0,0 +1,5 @@
+class CreatePostService {
+    execute() {}
+}
+
+export { CreatePostService }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,24 +1,24 @@
-import { Router } from 'express'
-import { UsersRepository } from '../repositories/UsersRepository'
-import { CreateUserService } from '../services/CreateUserService'
+import { Router } from "express";
+import { UsersRepository } from "../modules/users/repositories/UsersRepository";
+import { CreateUserService } from "../modules/users/services/CreateUserService";
 
-const usersRoutes = Router()
-const usersRepository = new UsersRepository()
+const usersRoutes = Router();
+const usersRepository = new UsersRepository();
 
 usersRoutes.post("/", (request, response) => {
-    const { name } = request.body
+  const { name } = request.body;
 
-    const createUserService = new CreateUserService(usersRepository)
+  const createUserService = new CreateUserService(usersRepository);
 
-    createUserService.execute(name)
+  createUserService.execute(name);
 
-    return response.status(201).send()
-})
+  return response.status(201).send();
+});
 
 usersRoutes.get("/", (request, response) => {
-    const all = usersRepository.list()
+  const all = usersRepository.list();
 
-    return response.json(all)
-})
+  return response.json(all);
+});
 
-export { usersRoutes }
+export { usersRoutes };
